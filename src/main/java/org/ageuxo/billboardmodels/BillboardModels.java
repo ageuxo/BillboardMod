3eac06912c2f7b9ee3960c697fce391920edd0cb
ageuxo <ageuxo@gmail.com>
2025-11-08 12:55:40 +0100

    Setup workspace

---
+++ src/main/java/org/ageuxo/billboardmodels/BillboardModels.java
@@ -0,0 +1,8 @@
+package org.ageuxo.billboardmodels;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(BillboardModels.MODID)
+public class BillboardModels {
+    public static final String MODID = "billboard_models";
+}
