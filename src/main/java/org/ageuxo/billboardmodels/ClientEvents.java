9f1a95f80f91b211737f1238ca5c8a53306e7426
ageuxo <ageuxo@gmail.com>
2025-11-08 22:18:28 +0100

    Add mixin config

---
+++ src/main/java/org/ageuxo/billboardmodels/ClientEvents.java
@@ -0,0 +1,9 @@
+package org.ageuxo.billboardmodels;
+
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = BillboardModels.MODID, value = Dist.CLIENT)
+public class ClientEvents {
+
+}
