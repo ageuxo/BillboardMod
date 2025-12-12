a83d1e57e317a8935e50f94aea490db8a334eb00
ageuxo <ageuxo@gmail.com>
2025-12-12 02:01:59 +0100

    Add tintable overload to renderSprite

--- src/main/java/org/ageuxo/billboardmodels/model/BillboardRenderer.java
+++ src/main/java/org/ageuxo/billboardmodels/model/BillboardRenderer.java
@@ -3,11 +3,13 @@ package org.ageuxo.billboardmodels.model;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.minecraft.client.Camera;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.LevelRenderer;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.server.packs.resources.ResourceManagerReloadListener;
+import net.minecraft.util.FastColor;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.Vec3;
@@ -42,21 +44,29 @@ public class BillboardRenderer implements ResourceManagerReloadListener {
         // Do rotation stuff here
         poseStack.mulPose(ROT);
         poseStack.translate(0, 0.5f, 0);
-        renderSprite(buf, poseStack, SPRITE_CACHE.computeIfAbsent(state, ClientHelper::getParticleSprite), light, OverlayTexture.NO_OVERLAY);
+        renderSprite(buf, poseStack, SPRITE_CACHE.computeIfAbsent(state, ClientHelper::getParticleSprite), light, OverlayTexture.NO_OVERLAY, 1, 1, 1);
         poseStack.popPose();
 
     }
 
-    public static void renderSprite(VertexConsumer buf, PoseStack poseStack, TextureAtlasSprite sprite, int packedLight, int packOverlay) {
-        addVert(poseStack.last(), buf, -0.5f, 0.5f, 0, sprite.getU0(), sprite.getV0(), packedLight, packOverlay);
-        addVert(poseStack.last(), buf, 0.5f, 0.5f, 0, sprite.getU1(), sprite.getV0(), packedLight, packOverlay);
-        addVert(poseStack.last(), buf, 0.5f, -0.5f, 0, sprite.getU1(), sprite.getV1(), packedLight, packOverlay);
-        addVert(poseStack.last(), buf, -0.5f, -0.5f, 0, sprite.getU0(), sprite.getV1(), packedLight, packOverlay);
+    public static void renderSprite(VertexConsumer buf, PoseStack poseStack, TextureAtlasSprite sprite, int packedLight, int packOverlay, int packedTint) {
+        int red = FastColor.ARGB32.red(packedTint);
+        int green = FastColor.ARGB32.green(packedTint);
+        int blue = FastColor.ARGB32.blue(packedTint);
+
+        renderSprite(buf, poseStack, sprite, packedLight, packOverlay, red, green, blue);
     }
 
-    private static void addVert(PoseStack.Pose pose, VertexConsumer buf, float x, float y, int z, float u, float v, int packedLight, int packOverlay) {
+    public static void renderSprite(VertexConsumer buf, PoseStack poseStack, TextureAtlasSprite sprite, int packedLight, int packOverlay, int red, int green, int blue) {
+        addVert(poseStack.last(), buf, -0.5f, 0.5f, 0, sprite.getU0(), sprite.getV0(), packedLight, packOverlay, red, green, blue);
+        addVert(poseStack.last(), buf, 0.5f, 0.5f, 0, sprite.getU1(), sprite.getV0(), packedLight, packOverlay, red, green, blue);
+        addVert(poseStack.last(), buf, 0.5f, -0.5f, 0, sprite.getU1(), sprite.getV1(), packedLight, packOverlay, red, green, blue);
+        addVert(poseStack.last(), buf, -0.5f, -0.5f, 0, sprite.getU0(), sprite.getV1(), packedLight, packOverlay, red, green, blue);
+    }
+
+    private static void addVert(PoseStack.Pose pose, VertexConsumer buf, float x, float y, int z, float u, float v, int packedLight, int packOverlay, float red, float green, float blue) {
         buf.vertex(pose.pose(), x, y, z)
-                .color(1f, 1f, 1f, 1f)
+                .color(red, green, blue, 1f)
                 .uv(u, v)
                 .uv2(packedLight)
                 .overlayCoords(packOverlay)
