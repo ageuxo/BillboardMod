9333f3495b0fee6cf0eda30d8d3871e5a8eca572
ageuxo <ageuxo@gmail.com>
2026-01-01 14:02:13 +0100

    Add BillboardBuilder to use when DataGen-ing Billboard models

---
+++ src/main/java/org/ageuxo/billboardmodels/datagen/BillboardBuilder.java
@@ -0,0 +1,81 @@
+package org.ageuxo.billboardmodels.datagen;
+
+import com.google.gson.JsonObject;
+import com.mojang.logging.LogUtils;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.JsonOps;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.client.model.generators.BlockModelBuilder;
+import net.minecraftforge.client.model.generators.CustomLoaderBuilder;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import org.ageuxo.billboardmodels.BillboardMod;
+import org.ageuxo.billboardmodels.model.SpriteGeometry;
+import org.joml.Vector2f;
+import org.slf4j.Logger;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BillboardBuilder extends CustomLoaderBuilder<BlockModelBuilder> {
+
+    public static final Logger LOGGER = LogUtils.getLogger();
+    private final List<SpriteGeometry.Sprite> sprites = new ArrayList<>();
+    private final Map<String, String> textures = new HashMap<>();
+
+    public BillboardBuilder(BlockModelBuilder parent, ExistingFileHelper existingFileHelper) {
+        super(BillboardMod.modRL("billboard"), parent, existingFileHelper);
+    }
+
+    public BillboardBuilder addTintedSprite(ResourceLocation sprite, int tintIndex, float originX, float originY, float offsetX, float offsetY) {
+        return addTintedSprite(sprite, new Vector2f(originX, originY), new Vector2f(offsetX, offsetY), tintIndex);
+    }
+
+    public BillboardBuilder addTintedSprite(ResourceLocation sprite, Vector2f origin, Vector2f offset, int tintIndex) {
+        String textureLocation = String.valueOf(textures.size());
+        textures.put(textureLocation, sprite.toString());
+        sprites.add(new SpriteGeometry.Sprite(textureLocation, origin, offset, tintIndex));
+
+        return this;
+    }
+
+    public BillboardBuilder addSprite(ResourceLocation sprite, float originX, float originY, float offsetX, float offsetY) {
+        return addSprite(sprite, new Vector2f(originX, originY), new Vector2f(offsetX, offsetY));
+    }
+
+    public BillboardBuilder addSprite(ResourceLocation sprite, Vector2f origin, Vector2f offset) {
+        String textureLocation = String.valueOf(textures.size());
+        textures.put(textureLocation, sprite.toString());
+        sprites.add(new SpriteGeometry.Sprite(textureLocation, origin, offset, -1));
+
+        return this;
+    }
+
+    public BillboardBuilder addParticleTexture(ResourceLocation particle) {
+        textures.put("particle", particle.toString());
+
+        return this;
+    }
+
+    @Override
+    public JsonObject toJson(JsonObject json) {
+        json.add("sprites",
+                SpriteGeometry.Sprite.CODEC.listOf().encodeStart(JsonOps.INSTANCE, sprites)
+                        .getOrThrow(false, LOGGER::error)
+        );
+        json.add("textures",
+                Codec.unboundedMap(Codec.STRING, Codec.STRING).encodeStart(JsonOps.INSTANCE, textures)
+                        .getOrThrow(false, LOGGER::error)
+        );
+        return super.toJson(json);
+    }
+
+    @Override
+    public BlockModelBuilder end() {
+        if (textures.get("particle") == null) {
+            throw new IllegalStateException("BillboardModelBuilder is missing particle texture! Bound textures:\n%s".formatted(textures));
+        }
+        return super.end();
+    }
+}
